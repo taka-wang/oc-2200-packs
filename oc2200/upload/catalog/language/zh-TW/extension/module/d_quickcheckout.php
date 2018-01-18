50173877fa7fe9fb747dd30f69dff9f6fa190d14
taka-wang <wang.chia.ming@gmail.com>
2018-01-19 01:57:13 +0800

    original ajax quick checkout

---
+++ oc2200/upload/catalog/language/zh-TW/extension/module/d_quickcheckout.php
@@ -0,0 +1,31 @@
+<?php 
+
+$_['title_payment_address']  						= '基本資料';
+$_['description_payment_address']  					= '';
+$_['title_shipping_address']  						= '出貨資料';
+$_['description_shipping_address'] 					= '';
+$_['title_shipping_method']  						= '出貨方式';
+$_['description_shipping_method'] 					= '';
+$_['title_payment_method'] 							= '付款方式';
+$_['description_payment_method'] 					= '';
+$_['title_shopping_cart'] 							= '訂單明細';
+$_['description_shopping_сart'] 					= '';
+$_['text_login']									= '會員登入';
+
+$_['placeholder_telephone'] 						= '* 手機';
+
+$_['error_field_required'] 							= '這個欄位必填';
+$_['error_valid_telephone'] 						= '請輸入有效的電話號碼';
+$_['entry_email_confirm'] 							= '確認 E-mail';
+$_['error_email_confirm'] 							= 'Email 不一致!';
+$_['entry_fax'] 									= '傳真:';
+
+$_['step_option_guest_desciption'] = '直接結帳不註冊帳號';
+
+$_['error_step_payment_address_fields_company'] = '公司名稱 3~128 個字';
+$_['error_step_shipping_address_fields_company'] = '公司名稱 3~128 個字';
+
+$_['error_step_confirm_fields_comment'] = '新增訂單備註';
+
+
+?>
